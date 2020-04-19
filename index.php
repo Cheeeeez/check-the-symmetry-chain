21d51eb2e6f6014ad57eea0f7539cf7c9737c8c2
Nam <nguyentrongnama8@gmail.com>
2020-04-20 02:37:09 +0700

    refactor

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
 include_once "Queue.php";
 include_once "Stack.php";
-
-function isSymmetryChain($str)
-{
-    $queue = new check_the_symmetry_chain\Queue();
-    $stack = new check_the_symmetry_chain\Stack();
-
-    for ($i = 0; $i < strlen($str); $i++) {
-        $queue->enqueue($str[$i]);
-        $stack->push($str[$i]);
-    }
-
-    for ($j = 0; $j < count($stack->getStack()); $j++) {
-        if ($stack->getStack()[$j] != $queue->readQueue()[$j]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function checkSymmetryChain($str)
-{
-    if (isSymmetryChain($str)) {
-        echo "<b>$str</b> là chuỗi đối xứng";
-    } else {
-        echo "<b>$str</b> không phải chuỗi đối xứng";
-    }
-}
+include_once "Function.php";
 
 checkSymmetryChain('ere');
 echo "<br>";
